755508d6b9c304d45a0777a355a9d2f2acf1aa5a
kendoctor <kendoctor@163.com>
2017-12-08 10:51:23 -0800

    Catch input from keyboard

--- hello/hello.php
+++ hello/hello.php
@@ -3,3 +3,12 @@
 //echo is a php function which can output text to the console or the browser.
 echo "Hello, welcome to php world.\n";
 
+//listen to the keyboard input and catch the input
+$handle =  fopen("php://stdin", "r");
+$input = fgets($handle);
+fclose($handle);
+
+//output the input, just like repeat what you said.
+echo $input;
+
+
