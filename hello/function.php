8d485c8d0ad8055dcab240f2be81e6365336d45f
kendoctor <kendoctor@163.com>
2017-12-10 11:02:11 -0800

    define your own functions

---
+++ hello/function.php
@@ -0,0 +1,51 @@
+<?php
+
+//we have seen many build-in functions in PHP before. 
+//we also can define your own functinons
+
+//speak out to the console 
+function speak($words)
+{
+	echo $words."\n";
+}
+
+//listen to the console
+function listen()
+{
+	$handle = fopen("php://stdin", "r");
+	$heard = fgets($handle);
+	fclose($handle);
+
+	return trim($heard);
+}
+
+//think question to get the answer
+function think($question)
+{
+	switch($question)
+	{
+		case "What's your name?":
+			return "I'm Jack";
+			break;
+		case "What's your job?":
+			return "I'm a developer.";
+			break;
+		case "Where are you from?":
+			return "I'm from American.";
+			break;
+		default: 
+			return "I have no idea about your question.";
+			break;
+	}
+
+}
+
+
+speak("What's your question?");
+
+$question = listen();
+
+$answer = think($question);
+
+speak($answer);
+
